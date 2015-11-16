4bee3b6624ec14699ab8696570f81fa8d8090ac0
willgabriel7 <williamgabriel04@gmail.com>
2015-11-15 22:48:49 -0300

    novo recurso
    Ori inicia parada

--- jogo.lua
+++ jogo.lua
@@ -46,6 +46,8 @@ local contadorTsurus = 0
 local tableTsurus = {}
 local indice = 1
 local trioTsurusAtual = 1
+local limiteNivel = 33
+local primeiraEscolha = true
 
 
 -- Funções
@@ -57,8 +59,6 @@ local fimDeJogo = {}
 local selecionarTsuru = {}
 local aumentarVelocidade = {}
 local update = {}
-local selecionarCor = {}
-local selecionarForma= {}
 local embaralhar = {}
 local removerTsurusNaoSelecionados = {}
 
@@ -100,7 +100,7 @@ function scene:show(event)
     adicionarOri()
 
     -- Adicionar tsurus
-    tsuruTimer = timer.performWithDelay(2000, adicionarTsurus, 0 )
+    tsuruTimer = timer.performWithDelay(3000, adicionarTsurus, 0 )
 
     -- Atualizar cena
     Runtime:addEventListener('enterFrame', update)
@@ -122,40 +122,40 @@ function scene:hide(event)
   end
 end
 
-function scene:destroy(event)
-  local sceneGroup = self.view
 
-  transition.cancel("all")
-  timer.cancel(tsuruTimer)
-  Runtime:removeEventListener("enterFrame", update)
-  Runtime:removeEventListener("touch", tsuru1)
-  Runtime:removeEventListener("touch", tsuru2)
-  Runtime:removeEventListener("touch", tsuru3)
-  display.remove(grupoImagens)
-  --display.remove(ori)
-  display.remove(textoDiferenciacao)
-  display.remove(etiquetaDiferenciacao)
-  display.remove(textoDistancia)
-  display.remove(etiquetaDistancia)
-    -- Chamado antes da remoção de vista da cena ("sceneGroup")
-    -- Código para "limpar" a cena
-    -- ex: remover obejtos display, save state, cancelar transições e etc
+-- Adiciona Ori
+function adicionarOri()
+  tsuru = display.newImage(caminhoDiretorioImagens .. "tsuru.png")
+  tsuru.x = LARGURA - 500
+  tsuru.y = ALTURA - 150
+  tsuru.color = ""
+  tsuru.shape = ""
+  physics.addBody(tsuru, "kinematic")
+
+  tsuru.touch = fimDeJogo
+
+  tsuru:addEventListener("collision", tsuru)
+
+  grupoImagens:insert(tsuru)
+
+  ori = display.newImage(caminhoDiretorioImagens .. 'ori.png')
+  ori.x = tsuru.x
+  ori.y = tsuru.y - 25
+  ori.name = "ori"
+  ori.isFixedRotation = true
+  physics.addBody(ori, "kinematic")
+  grupoImagens:insert(ori)
 end
 
--- Aumenta a distância
-function aumentarDistancia()
-     --incrementando a distância
-      distancia = distancia + 60
-      textoDistancia.text = string.format("%d m", distancia)
-end
-  --dtc = timer.performWithDelay( 1000, aumentarDistancia, 0 )
 
---Add Tsurus
+-- Adicionar Tsurus
 function adicionarTsurus(event)
     local tsurus = {}
   --Speed up each 15 tsurus added
-    if(event.count % 3 == 0 ) then
-    --  aumentarVelocidade()
+  print("contador = " .. event.count)
+    if(contadorTsurus == limiteNivel) then
+      limiteNivel = limiteNivel + 33
+      aumentarVelocidade()
     end
 
     embaralhar(cores)
@@ -169,8 +169,8 @@ function adicionarTsurus(event)
     local shape2 = formas[2]
     local shape3 = formas[3]
 
-    --Add 3 tsurus
-    --Add tsuru on top
+    --Adiciona três tsurus
+    --Adiciona tsuru no topo
     tsuru1 = display.newRect(0,0,80,80)
     tsuru1.fill = {type="image", filename=caminhoDiretorioImagens .. "tsurus/tsuru_".. color1 .. "_" .. shape1 .. ".png"}
     tsuru1.x = LARGURA
@@ -178,9 +178,9 @@ function adicionarTsurus(event)
     tsuru1.color = color1
     tsuru1.shape = shape1
     tsuru1.id = 1
-    physics.addBody(tsuru1, "static")
+    physics.addBody(tsuru1, "kinematic")
 
-    --Add tsuru in the middle
+    --Adiciona tsuru no meio
     tsuru2 = display.newRect(0,0,80,80)
     tsuru2.fill = {type="image", filename=caminhoDiretorioImagens .. "tsurus/tsuru_".. color2 .. "_" .. shape2 .. ".png"}
     tsuru2.x = LARGURA
@@ -188,9 +188,9 @@ function adicionarTsurus(event)
     tsuru2.color = color2
     tsuru2.shape = shape2
     tsuru2.id = 2
-    physics.addBody(tsuru2, "static")
+    physics.addBody(tsuru2, "kinematic")
 
-    --Add tsuru at the bottom
+    --Adiona tsuru ao fundo
     tsuru3 = display.newRect(0,0,80,80)
     tsuru3.fill = {type="image", filename=caminhoDiretorioImagens .. "tsurus/tsuru_" .. color3 .. "_" .. shape3 .. ".png"}
     tsuru3.x = LARGURA
@@ -198,14 +198,14 @@ function adicionarTsurus(event)
     tsuru3.color = color3
     tsuru3.shape = shape3
     tsuru3.id = 3
-    physics.addBody(tsuru3, "static")
+    physics.addBody(tsuru3, "kinematic")
 
-    --transition tsurus
+    --transição tsurus
     transition.to(tsuru1, {time = velocidade, x = -150, y = tsuru1.y, tag="all"})
     transition.to(tsuru2, {time = velocidade, x = -150, y = tsuru2.y, tag="all"})
     transition.to(tsuru3, {time = velocidade, x = -150, y = tsuru3.y, tag="all"})
 
-    --Add handler on touch
+    --Adiciona manipulador ao toque
     tsuru1.touch = selecionarTsuru
     tsuru2.touch = selecionarTsuru
     tsuru3.touch = selecionarTsuru
@@ -214,147 +214,100 @@ function adicionarTsurus(event)
     tsuru2:addEventListener("touch", tsuru2)
     tsuru3:addEventListener("touch", tsuru3)
 
+    --adiciona tsurus a um grupo
     grupoImagens:insert(tsuru1)
     grupoImagens:insert(tsuru2)
     grupoImagens:insert(tsuru3)
 
+    -- adiciona os três tsurus a uma table
     tsurus = {tsuru1, tsuru2, tsuru3}
 
+    -- adiciona a table com os três tsurus a outra table
     tableTsurus[indice] = tsurus
+
+    --[[ incrementa o indice, indicando em qual posicão da tabela os próximos três
+    tsurus serão adicionados]]
     indice = indice + 1
 
+    -- guarda o numero de tsurus adicionados
     contadorTsurus = contadorTsurus + 3
-    --show speed
-    --textoDistancia.text = speed
 end
 
--- generate random color
-function selecionarCor()
-  local color =  math.random(1, 3)
 
-  if(color == 1 and color ~= lastColor) then
-    color  = "red"
-  elseif (color == 2 and color ~= lastColor) then
-    color = "yellow"
-  else
-    color = "green"
-  end
-
-  lastColor = color
-
-  return color
-end
-
-function embaralhar( t )
-    local rand = math.random
-    local iterations = #t
+-- Embaralha itens de uma table
+function embaralhar(t)
     local j
 
-    for i = iterations, 2, -1 do
-        j = rand(i)
+    for i = #t, 2, -1 do
+        j = math.random(i)
         t[i], t[j] = t[j], t[i]
     end
 end
 
--- generate random shape
-function selecionarForma()
-  local shape =  math.random(1, 3)
 
-  if(shape == 1 and shape ~= lastShape) then
-    shape  = "shape1"
-  elseif (color == 2 and shape ~= lastShape) then
-    shape = "shape2"
-  else
-    shape = "shape3"
-  end
-
-  lastShape = shape
-
-  return shape
-end
-
---Game's speed up
-function aumentarVelocidade()
-  velocidade = velocidade - 1000
-
-  transition.cancel(ori)
-  transition.to(ori, {time = velocidade - 2000, x = -150, y = ori.y , tag="all"})
-end
-
-function update()
-  -- utilizar o tamanho da tela do dispositivo LARGURA
-  if(ori.x < -75 or contadorTsurus == 1000) then
-    --mudar para composer.fimDeJogo()
-    fimDeJogo()
-  end
-
-  --.text  = distance
-end
-
---Add ori to game
-function adicionarOri()
-  tsuru = display.newImage(caminhoDiretorioImagens .. "tsuru.png")
-  tsuru.x = LARGURA
-  tsuru.y = ALTURA - 150
-  tsuru.color = ""
-  tsuru.shape = ""
-  physics.addBody(tsuru, "static")
-
-  grupoImagens:insert(tsuru)
-
-  --ultimaCorSelecionada = tsuru.name
-
-  ori = display.newImage(caminhoDiretorioImagens .. 'ori.png')
-  ori.x = tsuru.x
-  ori.y = tsuru.y - 25
-  ori.name = "ori"
-  ori.isFixedRotation = true
-  physics.addBody(ori, "static")
-  --ori.linearDamping = 5
-    grupoImagens:insert(ori)
-
- transition.to(ori, {time = velocidade, x = -150, y = ori.y, tag="all"})
-
-  --tsuru.collision = selecionarTsuru
-
---  tsuru:addEventListener("collision", tsuru)
-
-  transition.to(tsuru, {time = velocidade, x = -150, y = tsuru.y, tag="all"})
-
-  --differentiation rule
-  diferenciar(tsuru)
-end
-
---Tsuru is touched
+-- Tsuru é tocado
 function selecionarTsuru(self, event)
 
-  --Moves Ori to the tsuru touched
-  if(event.phase == "began" and self.x > ori.x and self.x < (ori.x + 150)) then
+  -- Move Ori para o tsuru tocado
+  if(event.phase == "began" and ((primeiraEscolha) or (self.x > ori.x and self.x < (ori.x + 250)))) then
+    primeiraEscolha = false
+
+    transition.to(tsuru, {time = 7000, x = -150, y = tsuru.y, tag="all"})
     ori.x = self.x
     ori.y = self.y - 25
 
     transition.to(ori, {time = velocidade, x = -150, y = ori.y, tag="all"})
 
-
     diferenciar(self)
 
     totalTsurusSaltados = totalTsurusSaltados + 1
+
     aumentarDistancia()
 
     removerTsurusNaoSelecionados(self.id)
 
-      self.fill = {type="image", filename=caminhoDiretorioImagens .. "tsuru.png"}
+    -- self.fill = {type="image", filename=caminhoDiretorioImagens .. "tsuru.png"}
   end
 end
+
+function diferenciar(TsuruAtual)
+  if(regraDiferenciacao == "cor") then
+    if(TsuruAtual.color == ultimaCorSelecionada) then
+      fimDeJogo()
+    end
+  else
+    if(TsuruAtual.shape == ultimaFormaSelecionada) then
+      fimDeJogo()
+    end
+  end
+
+  ultimaCorSelecionada = TsuruAtual.color
+  ultimaFormaSelecionada = TsuruAtual.shape
+
+  mudarRegraDiferenciacao()
+end
+
+
+--Change the tsurus differentiation mode
+function mudarRegraDiferenciacao()
+  local randomNumber = math.random(0,1)
+
+  if(randomNumber == 0) then
+    regraDiferenciacao = "cor"
+  else
+    regraDiferenciacao = "forma"
+  end
+
+  textoDiferenciacao.text = string.format("%s", regraDiferenciacao)
+
+end
+
 
 -- Remove os tsurus não selecionados do trio de tsurus atual
 function removerTsurusNaoSelecionados(tsuruId)
   local tsuruNaoSelecionado1
   local tsuruNaoSelecionado2
---[[  print("tsuro1 removido")
-  tsuruNaoSelecionado1 = print(table.remove(tableTsurus[trioTsurusAtual],1))
-  print("tssuro2 removido")
-  tsuruNaoSelecionado2 = print(table.remove(tableTsurus[trioTsurusAtual],2))]]
+
   if (tsuruId == 1) then
     tsuruNaoSelecionado2 = table.remove(tableTsurus[trioTsurusAtual],3)
     tsuruNaoSelecionado2:removeSelf()
@@ -385,64 +338,56 @@ function removerTsurusNaoSelecionados(tsuruId)
   trioTsurusAtual =  trioTsurusAtual + 1
 end
 
--- muda cor e forma do tsuru selecionado para cinza
-function mudarCorEFormaTsuru(self)
-  self:removeSelf()
 
+-- Aumenta a distância
+function aumentarDistancia()
+     --incrementando a distância
+      distancia = distancia + 60
+      textoDistancia.text = string.format("%d m", distancia)
 end
 
---Tsurus differentiation
-function diferenciar(TsuruAtual)
-  if(regraDiferenciacao == "cor") then
-    if(TsuruAtual.color == ultimaCorSelecionada) then
-      fimDeJogo()
-    end
-  else
-    if(TsuruAtual.shape == ultimaFormaSelecionada) then
-      fimDeJogo()
-    end
+
+-- Aumenta a velocidade
+function aumentarVelocidade()
+  velocidade = velocidade - 1000
+end
+
+function update()
+  -- utilizar o tamanho da tela do dispositivo LARGURA
+  if(ori.x < LARGURA - 700 or contadorTsurus == 1000) then
+    fimDeJogo()
   end
-
-  ultimaCorSelecionada = TsuruAtual.color
-  ultimaFormaSelecionada = TsuruAtual.shape
-
-  mudarRegraDiferenciacao()
-end
-
---Change the tsurus differentiation mode
-function mudarRegraDiferenciacao()
-  local randomNumber = math.random(0,1)
-
-  if(randomNumber == 0) then
-    regraDiferenciacao = "cor"
-  else
-    regraDiferenciacao = "forma"
-  end
-
-  textoDiferenciacao.text = string.format("%s", regraDiferenciacao)
-
 end
 
 
---------------------------------------------------------------------------------
 -- Configuração de transição entre cenas
---------------------------------------------------------------------------------
 local transicaofimDeJogoConfig = {
 	effect = "fade", time = 1000
 }
---------------------------------------------------------------------------------
 
 
---------------------------------------------------------------------------------
 -- Função que chama cena para início do jogo
---------------------------------------------------------------------------------
 function fimDeJogo()
   composer.removeScene("jogo")
   composer.gotoScene("fim_de_jogo", transicaofimDeJogoConfig)
 end
 
+function scene:destroy(event)
+  local sceneGroup = self.view
 
---------------------------------------------------------------------------------
+  transition.cancel("all")
+  timer.cancel(tsuruTimer)
+  Runtime:removeEventListener("enterFrame", update)
+  Runtime:removeEventListener("touch", tsuru1)
+  Runtime:removeEventListener("touch", tsuru2)
+  Runtime:removeEventListener("touch", tsuru3)
+  display.remove(grupoImagens)
+  display.remove(textoDiferenciacao)
+  display.remove(etiquetaDiferenciacao)
+  display.remove(textoDistancia)
+  display.remove(etiquetaDistancia)
+end
+
 
 -- Listener Setup
 scene:addEventListener("create", scene)
@@ -450,6 +395,5 @@ scene:addEventListener("show", scene)
 scene:addEventListener("hide", scene)
 scene:addEventListener("destroy", scene)
 
---------------------------------------------------------------------------------
 
 return scene
